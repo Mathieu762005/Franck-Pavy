81bb61477d94fc9725e88b860d84072c38d2dab8
Macbook <macbook@MacBook-Air-de-Macbook.local>
2025-12-06 19:01:49 +0100

    maj navbar style

--- src/Views/template/navbar.php
+++ src/Views/template/navbar.php
@@ -1,17 +1,26 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<head>
+    <link rel="stylesheet" href="../assets/css/navbar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+<nav class="navbar navbar-expand-lg background">
     <div class="container-fluid">
-        <a class="navbar-brand" href="http://localhost:8000/index.php?url=01_home">logo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
+        <div class="collapse navbar-collapse d-flex" id="navbarScroll">
+            <ul>
+                <a class="navbar-brand" href="http://localhost:8000/index.php?url=01_home">
+                    <img src="assets/image/Logo/Logo.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                    aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </ul>
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/index.php?url=01_home">Home</a>
+                    <a class="nav-link color" href="http://localhost:8000/index.php?url=01_home">Home</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown"
+                    <a class="nav-link dropdown-toggle color" href="" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         Produits
                     </a>
@@ -27,18 +36,20 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/index.php?url=03_a_propos">A propos</a>
+                    <a class="nav-link color" href="http://localhost:8000/index.php?url=03_a_propos">A propos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/index.php?url=05_contact">Contact</a>
+                    <a class="nav-link color" href="http://localhost:8000/index.php?url=05_contact">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/index.php?url=04_click_and_collect">Click And
+                    <a class="nav-link color" href="http://localhost:8000/index.php?url=04_click_and_collect">Click And
                         Collect</a>
                 </li>
+            </ul>
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <?php if (!isset($_SESSION['user']['id'])): ?>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        <a class="nav-link dropdown-toggle color" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             Compte
                         </a>
@@ -49,16 +60,18 @@
                     </li>
                 <?php endif; ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/index.php?url=06_profil">Profil</a>
+                    <a class="nav-link" href="http://localhost:8000/index.php?url=06_profil"><i
+                            class="bi bi-person-circle color"></i></a>
                 </li>
                 <?php if (isset($_SESSION['user']) && $_SESSION['user']['role'] === 'admin'): ?>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        <a class="nav-link dropdown-toggle color" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             Admin
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="http://localhost:8000/index.php?url=adminUsers">Gestion des
+                            <li><a class="dropdown-item" href="http://localhost:8000/index.php?url=adminUsers">Gestion
+                                    des
                                     Gestion des utilisateurs</a></li>
                             <li><a class="dropdown-item" href="http://localhost:8000/index.php?url=adminMessages">Gestion
                                     Gestion des messages</a></li>
@@ -71,13 +84,12 @@
                 <?php endif; ?>
                 <?php if (isset($_GET['url']) && $_GET['url'] === '04_click_and_collect'): ?>
                     <li>
-                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
+                        <button class="btn btn-primary color" type="button" data-bs-toggle="offcanvas"
                             data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                             Panier
                         </button>
                     </li>
                 <?php endif; ?>
-
             </ul>
         </div>
     </div>
