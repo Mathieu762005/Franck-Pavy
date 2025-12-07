453de407bb90e8aa7c2110a5378803870848f307
Macbook <macbook@MacBook-Air-de-Macbook.local>
2025-12-07 19:44:40 +0100

    maj navbar style

--- src/Views/04_click_and_collect.php
+++ src/Views/04_click_and_collect.php
@@ -13,11 +13,12 @@ $showLoginModal = !$connected;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Click & Collect</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
 </head>
 
 <body>
 <header>
-    <?php include_once "template/navbar.php"; ?>
+    <?php include_once "template/navbarC&C.php"; ?>
 
     <!-- Offcanvas du panier -->
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
