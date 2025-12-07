1d39020912fa79d1a2a687f60e536e0787c68eb3
Macbook <macbook@MacBook-Air-de-Macbook.local>
2025-12-07 19:44:40 +0100

    maj navbar style

--- src/Views/template/footer.php
+++ src/Views/template/footer.php
@@ -1,3 +1,11 @@
+<div class="text-black">
+    <div class="">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2588.75778905509!2d0.19044397724887885!3d49.545723352090796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e03abf207c051f%3A0x19f908d7f9b7272!2s62%20Rue%20F%C3%A9lix%20Faure%2C%2076290%20Montivilliers!5e0!3m2!1sfr!2sfr!4v1765131683511!5m2!1sfr!2sfr"
+            width="300" height="225" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
 <?php if (isset($_SESSION['user'])): ?>
-<a class="btn border border-black" href="index.php?url=logout" type="submit">déconnexion</a>
+    <a class="btn border border-black" href="index.php?url=logout" type="submit">déconnexion</a>
 <?php endif; ?>
--- src/Views/template/navbarC&C.php
+++ src/Views/template/navbarC&C.php
@@ -1,6 +1,6 @@
 <head>
-    <link rel="stylesheet" href="../assets/css/navbar.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
 </head>
 
 <nav class="navbar navbar-expand-lg background">
@@ -24,29 +24,19 @@
             </ul>
 
             <!-- CENTRE 40% -->
-            <ul class="navbar-nav mx-auto justify-content-evenly" style="width:40%;">
-                <li class="nav-item">
-                    <a class="nav-link color" href="index.php?url=01_home">Home</a>
+            <ul class="navbar-nav mx-auto flex-row align-items-center gap-3">
+                <li class="nav-item d-flex align-items-center gap-1 color">
+                    <i class="bi bi-phone-fill" style="font-size: 1.5rem;"></i>
+                    <a href="tel:0235302758" class="mon-lien">02 35 30 27 58</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle color" href="#" data-bs-toggle="dropdown">
-                        Produits
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="index.php?url=02_produits&id=1">Les Pains</a></li>
-                        <li><a class="dropdown-item" href="index.php?url=02_produits&id=2">Les Viennoiseries</a></li>
-                        <li><a class="dropdown-item" href="index.php?url=02_produits&id=3">Pause Déjeuner</a></li>
-                        <li><a class="dropdown-item" href="index.php?url=02_produits&id=4">Les Pâtisseries</a></li>
-                    </ul>
-                </li>
+
                 <li class="nav-item">
-                    <a class="nav-link color" href="index.php?url=03_a_propos">A propos</a>
+                    <span class="mx-2 colorBarre" style="color: #571065; font-size: 1.5rem;"> | </span>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link color" href="index.php?url=05_contact">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link color" href="index.php?url=04_click_and_collect">Click & Collect</a>
+
+                <li class="nav-item d-flex align-items-center gap-1 color">
+                    <i class="bi bi-geo-alt-fill" style="font-size: 1.5rem;"></i>
+                    <span class="mon-lien" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Montivilliers</span>
                 </li>
             </ul>
 
@@ -94,3 +84,27 @@
         </div>
     </div>
 </nav>
+
+
+<!-- Vertically centered modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">La boulangerie À Toute Heure a Montivilliers</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2588.75778905509!2d0.19044397724887885!3d49.545723352090796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e03abf207c051f%3A0x19f908d7f9b7272!2s62%20Rue%20F%C3%A9lix%20Faure%2C%2076290%20Montivilliers!5e0!3m2!1sfr!2sfr!4v1765131683511!5m2!1sfr!2sfr"
+                    width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+            </div>
+        </div>
+    </div>
+</div>
